abe8fed852428ef3cb5f99340a846136c3272a35
Josh Roberts <me@syn.fyi>
2024-12-20 14:00:41 +0000

    add basic index page

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const LandingPage = () => {
+    return (
+        <div style={{ textAlign: 'center', marginTop: '20%' }}>
+            <h1>Fastview360 Link Shortening Service</h1>
+            <p>No public access</p>
+        </div>
+    );
+};
+
+export default LandingPage;

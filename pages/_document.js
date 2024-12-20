487f93751feda34b72a8b864d8577ee13f3fece9
Josh Roberts <me@syn.fyi>
2024-12-20 14:00:47 +0000

    change page title

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <title>Magic Teleport</title>
+      <title>Fastview360 Link Shortener</title>
       <link rel="stylesheet" href="https://cdn.1998.media/css/fontawesome.css" />
       <body>
         <Main />
